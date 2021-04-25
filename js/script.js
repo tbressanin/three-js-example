167e39037c7364d05584baa1614e00880fd32e0c
bressawsk <thiago@freecode.com.br>
2021-04-25 15:49:58 -0300

    Add files

---
+++ js/script.js
@@ -0,0 +1,36 @@
+import * as THREE from './three/build/three.module.js';
+ 
+let camera, scene, renderer;
+let geometry, material, mesh;
+ 
+init();
+animate();
+ 
+function init() {
+ 
+    camera = new THREE.PerspectiveCamera( 120, window.innerWidth / window.innerHeight, 0.01, 10 );
+    camera.position.z = 1;
+ 
+    scene = new THREE.Scene();
+ 
+    geometry = new THREE.BoxGeometry( 0.8, 0.8, 0.8);
+    material = new THREE.MeshNormalMaterial();
+ 
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+ 
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+ 
+}
+ 
+function animate() { 
+    requestAnimationFrame(animate);
+ 
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+ 
+    renderer.render( scene, camera ); 
+    
+}
